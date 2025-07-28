66205eec7b590bc0f693b37990c8eefbafbf7099
Sahil Yadav <150715924+NIRLAJ@users.noreply.github.com>
2025-07-28 12:53:03 +0000

    var

---
+++ 01_basics/var.js
@@ -0,0 +1,9 @@
+const accountnumber = 1234567890;
+let email = "sahil@gmail.com";
+var name = "Sahil";
+accountcity = "Delhi";
+console.log(accountnumber);
+email="okok@gmail.com";
+name = "Sahil Kumar";
+accountcity = "Mumbai";
+console.table([email, name, accountcity]);
