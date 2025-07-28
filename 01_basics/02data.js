d6f42f286fa3f66b55f6668ad4022433e5ccedf3
Sahil Yadav <150715924+NIRLAJ@users.noreply.github.com>
2025-07-28 12:50:15 +0000

    datatypes

---
+++ 01_basics/02data.js
@@ -0,0 +1,5 @@
+"use  strict";
+alert("Hello World");//we are using node.js not the browser, so this will not work
+//null=> ek standalone value h 
+//bake sare normal datatyyyes like boolean string int bigint float etc
+console.log(typeof "sahil");//string
