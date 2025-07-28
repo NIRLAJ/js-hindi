3973fa6195314eacf66a902bbf09e646458a3bd5
Sahil Yadav <150715924+NIRLAJ@users.noreply.github.com>
2025-07-28 13:17:06 +0000

    conversion of data type

---
+++ 01_basics/03conver.js
@@ -0,0 +1,9 @@
+let score="33avc"
+console.log(typeof score); //string
+let value=Number(score);
+console.log(typeof value); //number 01_basics/03conver.js
+console.log(value); //NaN
+let  islog=1;
+let boolc=Boolean(islog);
+console.log(boolc); //true
+//""=> false
