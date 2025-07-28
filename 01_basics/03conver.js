61991eae97a5adb81ee231bd0969aa444dc0ebd0
Sahil Yadav <150715924+NIRLAJ@users.noreply.github.com>
2025-07-28 13:35:10 +0000

    operation

--- 01_basics/03conver.js
+++ 01_basics/03conver.js
@@ -7,3 +7,22 @@ let  islog=1;
 let boolc=Boolean(islog);
 console.log(boolc); //true
 //""=> false
+//operation****************************************
+
+let num=2;
+let negnum=-num;
+console.log(negnum); //-2
+//basic operations
+/*console.log(2+3); //5
+console.log(2-3); //-1
+console.log(2*3); //6
+console.log(2/3); //0.6666666666666666
+console.log(2%3); //2
+console.log(2**3); //8power
+*/
+let str1="sahil";
+let str2=" kumar";
+let str3=str1+str2;
+console.log(str3); //sahilkumar
+console.log("1"+2+2); //122
+console.log(1+2+"2"); //32
